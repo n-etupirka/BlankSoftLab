28b0d7ba49dda5610713c7d9f9d94df4f1d08d31
Nao Komatsu <riiiin0806@gmail.com>
2020-12-05 16:26:23 +0900

    first commit

---
+++ Install-BlankSoftLab.ps1
@@ -0,0 +1,35 @@
+﻿# ----------------------------------------------------------
+# Global Settings
+# ----------------------------------------------------------
+$LabName = "BlankSoftLab"
+$DomainName = "blank-software.local"
+$DomainAdminUser = "sysadmin"
+$DomainAdminPassword = "P@ssw0rd123!"
+$ServerMemory = 2GB
+$ClientMemory = 4GB
+
+# ----------------------------------------------------------
+# Lab Install
+# ----------------------------------------------------------
+New-LabDefinition -Name $LabName -DefaultVirtualizationEngine HyperV
+
+Set-LabInstallationCredential -Username $DomainAdminUser -Password $DomainAdminPassword
+
+Add-LabDomainDefinition -Name $DomainName -AdminUser $DomainAdminUser -AdminPassword $DomainAdminPassword
+
+Add-LabMachineDefinition `
+        -Name "DC01" `
+        -DomainName $DomainName `
+        -Roles RootDC `
+        -OperatingSystem 'Windows Server 2019 Standard Evaluation (Desktop Experience)' `
+        -Memory $ServerMemory
+
+Add-LabMachineDefinition `
+        -Name "WS01" `
+        -DomainName $DomainName `
+        -OperatingSystem 'Windows 10 Enterprise Evaluation' `
+        -Memory $ClientMemory
+
+Install-Lab
+
+Show-LabDeploymentSummary -Detailed
