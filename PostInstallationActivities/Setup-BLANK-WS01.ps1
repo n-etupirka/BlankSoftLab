0f3a2a4c3409623be8b88f3df5b7b308908540a3
Nao Komatsu <riiiin0806@gmail.com>
2020-12-09 16:50:12 +0900

    fix: Post installation activities

--- PostInstallationActivities/Setup-BLANK-WS01.ps1
+++ PostInstallationActivities/Setup-BLANK-WS01.ps1
@@ -1,4 +1,4 @@
-﻿sStart-Transcript -Path C:\Windows\Temp\postinstallation.log -append
+﻿Start-Transcript -Path C:\Windows\Temp\postinstallation.log -append
 
 # Add oka_sakura to Administrators
 Add-LocalGroupMember -Group Administrators -Member blank-software\oka_sakura
